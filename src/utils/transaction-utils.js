d5a7c08d1dfeb4175c627d37ced78ba92df33e60
Nick Borsuk <gor_nick987@Mail.ru>
2019-09-10 13:24:26 +0300

    [ECHOJS-150] add encoder and decoder for ETH tx hash

--- src/utils/transaction-utils.js
+++ src/utils/transaction-utils.js
@@ -1,8 +1,8 @@
 import { constants } from "echojs-lib";
 import { shortMemoToAddress } from './address-utils';
-import { weiValueToAssert } from './converters-utils';
+import { addHexPrefix, weiValueToAssert } from './converters-utils';
 import { NATHAN_ID } from '../constants/echo-constants'
-import { isValidData } from './validators';
+import { isValidData, isValidTransactionHash } from './validators';
 
 /**
  *
@@ -57,4 +57,38 @@ export const mapEthTxToEcho = (ethTx, asset) => {
 	}
 
 	return { options, operationId };
+};
+
+/**
+ * @description encode echo broadcasting result to hash
+ * hash structure: 0x[operationId 1b][blockNumber 4b][transactionIndex 2b]00000000000000000000000000000000000000000000000000
+ * @param {BroadcastingResult} broadcastingResult
+ * @return {String}
+ */
+export const encodeTxHash = (broadcastingResult) => {
+	const hashBuffer = Buffer.alloc(32);
+	const { block_num: blockNumber, trx_num: txIndex, trx: transactionInfo } = broadcastingResult;
+	const [[operationId]] = transactionInfo.operations;
+	hashBuffer.writeUInt8(operationId, 0);
+	hashBuffer.writeUInt32LE(blockNumber, 1);
+	hashBuffer.writeUInt16LE(txIndex, 5);
+	return addHexPrefix(hashBuffer.toString('hex'));
+};
+
+/**
+ * decode hash string to echo parameters for transaction identification
+ * @param {String} hash
+ * @return {{operationId: number, blockNumber: number, txIndex: number}}
+ */
+export const decodeTxHash = (hash) => {
+	if (!isValidTransactionHash(hash)) throw new Error(`invalid tx hash string: ${hash}`);
+	if(!/0{50}$/.test(hash)) throw new Error('invalid hex 64 symbols hash string');
+	const hashBuffer = Buffer.from(hash.slice(2), 'hex');
+	const operationId = hashBuffer.readUInt8(0);
+	if (Object.values(constants.OPERATIONS_IDS).indexOf(operationId) === -1) {
+		throw new Error(`unknown operationId ${operationId}`);
+	}
+	const blockNumber = hashBuffer.readUInt32LE(1);
+	const txIndex = hashBuffer.readUInt16LE(5);
+	return { operationId, blockNumber, txIndex };
 };
