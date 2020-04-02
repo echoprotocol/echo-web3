b3e0ad5b7c02ec07b1dc13b6a31363e99420488d
Ladutko Sergey <s.ladutko@pixelplex.io>
2020-04-02 10:48:17 +0300

    [ECHOT-340] add publish to npm

---
+++ publish.sh
@@ -0,0 +1,13 @@
+ 
+#!/bin/bash
+VERSION=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/-/ /' | awk '{print $2}')
+if  cat package.json| grep version | grep echo > /dev/null
+then
+ECHOTEG=$(cat package.json | grep version | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/\-/\ /g' | awk '{print $2}')
+npm publish --tag "$ECHOTEG"
+elif [ -z "$VERSION" ]
+then
+npm publish
+else
+npm publish --tag "rc"
+fi
